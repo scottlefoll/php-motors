2ae734249c712671ee8724fcdb7964d64563ae14
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2023-01-28 19:26:49 -0500

    update

--- view/500.php
+++ view/500.php
@@ -2,16 +2,6 @@
 
 <html lang="en">
 
-<?php   
-        if(isset($_GET['page'])){
-            $current_page = $_GET['page'];
-            if ($current_page != '500') {
-                require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/library/connections.php';
-            }
-        } else {
-            require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/library/connections.php';
-        }
-    ?>
     <!-- Head -->
     <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/head.php'; ?>
 
@@ -24,12 +14,7 @@
             <main>
             <br>
             <?php
-                    if(isset($_GET['page'])){
-                        $current_page = $_GET['page'];
-                        require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/' . $current_page . '_content.php';
-                    }else{
-                        require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/home_content.php';
-                    }
+                        require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/500_content.php';
             ?>
             
                 <p><br><br><br><br><br><br><br></p>
